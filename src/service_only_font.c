6c1b2118bfc7498b80b59fb2bbbe77639ccfccb5
Jingnan Si <jingnan.si@gmail.com>
2018-11-19 11:03:34 -0800

    add more font functions

--- src/service_only_font.c
+++ src/service_only_font.c
@@ -5,6 +5,7 @@
 
 #include "lisp.h"
 #include "service_only_term.h"
+#include "service_only_font.h"
 #include "frame.h"
 #include "coding.h"	/* for ENCODE_SYSTEM, DECODE_SYSTEM */
 
@@ -42,17 +43,109 @@ sofont_get_cache (struct frame *f)
   return (dpyinfo->name_list_element);
 }
 
+static Lisp_Object
+sofont_list (struct frame *f, Lisp_Object font_spec) {
+  return Qnil;
+}
+
+static
+Lisp_Object
+intern_font_name (const char * string)
+{
+  Lisp_Object str = DECODE_SYSTEM (build_string (string));
+  ptrdiff_t len = SCHARS (str);
+  Lisp_Object obarray = check_obarray (Vobarray);
+  Lisp_Object tem = oblookup (obarray, SSDATA (str), len, len);
+  /* This code is similar to intern function from lread.c.  */
+  return SYMBOLP (tem) ? tem : intern_driver (str, obarray, tem);
+}
+
+static Lisp_Object
+sofont_match (struct frame *f, Lisp_Object font_spec) {
+  Lisp_Object entity;
+
+  entity = font_make_entity ();
+
+  ASET (entity, FONT_TYPE_INDEX, Qsofont);
+  ASET (entity, FONT_REGISTRY_INDEX, Qnil);
+  ASET (entity, FONT_OBJLIST_INDEX, Qnil);
+
+  ASET (entity, FONT_FOUNDRY_INDEX, Qunknown);
+
+  /* Save the generic family in the extra info, as it is likely to be
+     useful to users looking for a close match.  */
+  /* generic_type = physical_font->ntmTm.tmPitchAndFamily & 0xF0; */
+  /* if (generic_type == FF_DECORATIVE) */
+  /*   tem = Qdecorative; */
+  /* else if (generic_type == FF_MODERN) */
+  /*   tem = Qmono; */
+  /* else if (generic_type == FF_ROMAN) */
+  /*   tem = Qserif; */
+  /* else if (generic_type == FF_SCRIPT) */
+  /*   tem = Qscript; */
+  /* else if (generic_type == FF_SWISS) */
+  /*   tem = Qsans; */
+  /* else */
+  /*   tem = Qnil; */
+
+  ASET (entity, FONT_ADSTYLE_INDEX, Qsans);
+  ASET (entity, FONT_SPACING_INDEX, make_fixnum (FONT_SPACING_CHARCELL));
+  ASET (entity, FONT_FAMILY_INDEX, intern_font_name ("Source Code Pro"));
+
+  FONT_SET_STYLE (entity, FONT_WEIGHT_INDEX,
+		  make_fixnum (80));
+  FONT_SET_STYLE (entity, FONT_SLANT_INDEX,
+		  make_fixnum (100));
+  FONT_SET_STYLE (entity, FONT_WIDTH_INDEX, make_fixnum (100));
+
+  ASET (entity, FONT_SIZE_INDEX, make_fixnum (0));
+
+  font_put_extra (entity, QCformat, intern ("truetype"));
+
+  return entity;
+}
+
+static Lisp_Object
+sofont_open (struct frame *f, Lisp_Object font_entity, int pixel_size)
+{
+  Lisp_Object font_object
+    = font_make_object (VECSIZE (struct sofont_info),
+                        font_entity, pixel_size);
+  struct sofont_info *so_font
+    = (struct sofont_info *) XFONT_OBJECT (font_object);
+  struct font * font = (struct font *)so_font;
+
+  ASET (font_object, FONT_TYPE_INDEX, Qsofont);
+
+  so_font->glyph_idx = 0;
+
+  font->space_width = font->average_width = 10;
+  font->vertical_centering = 0;
+  font->baseline_offset = 0;
+  font->relative_compose = 0;
+  font->default_ascent = 0.1;
+  font->pixel_size = pixel_size;
+  font->driver = &sofont_driver;
+  font->encoding_charset = -1;
+  font->repertory_charset = -1;
+  font->min_width = font->space_width;
+  font->ascent = 0.1;
+  font->descent = 0.9;
+  font->height = pixel_size;
+
+  return font_object;
+}
 
 struct font_driver sofont_driver =
   {
     LISPSYM_INITIALLY (Qsofont),
     false, /* case insensitive */
     sofont_get_cache,
-    NULL, // sofont_list,
-    NULL, // sofont_match,
+    sofont_list,
+    sofont_match,
     NULL, // sofont_list_family,
     NULL, /* free_entity */
-    NULL, // sofont_open,
+    sofont_open,
     NULL, // sofont_close,
     NULL, /* prepare_face */
     NULL, /* done_face */
---
+++ src/service_only_font.h
@@ -0,0 +1,12 @@
+#ifndef __SERVICE_ONLY_FONT_H__
+#define __SERVICE_ONLY_FONT_H__
+
+#include "font.h"
+
+struct sofont_info
+{
+  struct font font;
+  unsigned int glyph_idx;
+};
+
+#endif //__FSERVICE_ONLY_FONT_H__
